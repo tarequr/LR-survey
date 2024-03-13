f10a57b14727d98222ff6104d043dcf0de390de6
tarequr <tarequrr8@gmail.com>
2024-03-13 22:30:32 +0600

    feat: creat signup,login request and AuthController

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SignupRequest;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+        //
+    }
+
+    public function login(LoginRequest $request)
+    {
+        //
+    }
+
+    public function logout()
+    {
+        //
+    }
+}
