5881a3d46613ad196b43f76e5fc5da91f0f6cfd0
tarequr <tarequrr8@gmail.com>
2024-03-13 23:02:53 +0600

    feat: logout method done

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -52,6 +52,11 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        //
+        $user = Auth::user();
+        $user->currentAccessToken->delete();
+
+        return response([
+            'success' => true,
+        ]);
     }
 }
