501979cf91e8248b62a6ce55e7d8883bd2e87e7d
tarequr <tarequrr8@gmail.com>
2024-03-13 22:49:59 +0600

    feat: signup method done

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,12 +6,27 @@ use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function signup(SignupRequest $request)
     {
-        //
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email'  => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token,
+        ]);
     }
 
     public function login(LoginRequest $request)
@@ -19,7 +34,7 @@ class AuthController extends Controller
         //
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
         //
     }
