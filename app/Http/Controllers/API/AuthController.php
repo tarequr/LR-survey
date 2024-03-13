cf10ce063c505bc72ae5fe932f24c3c4a6d8d544
tarequr <tarequrr8@gmail.com>
2024-03-13 22:59:31 +0600

    feat: login method done

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SignupRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -31,7 +32,22 @@ class AuthController extends Controller
 
     public function login(LoginRequest $request)
     {
-        //
+        $credentials = $request->validated();
+        $remember = $credentials['remember'] ?? false;
+
+        if (!Auth::attempt($credentials, $remember)) {
+            return response([
+                'error' => 'The credentials are invalid'
+            ], 422);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token,
+        ]);
     }
 
     public function logout(Request $request)
