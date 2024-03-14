3cd8676bf49269e04f53b85b9510f10b318706df
tarequr <tarequrr8@gmail.com>
2024-03-14 22:19:32 +0600

    signup account design done

--- react/src/view/Signup.jsx
+++ react/src/view/Signup.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Signup() {
   return (
@@ -8,6 +9,23 @@ function Signup() {
       </h2>
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form className="space-y-6" action="#" method="POST">
+
+        <div>
+            <label htmlFor="full-name" className="block text-sm font-medium leading-6 text-gray-900">
+              Full Name
+            </label>
+            <div className="mt-2">
+              <input
+                id="full-name"
+                name="name"
+                type="text"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder='Enter full name'
+              />
+            </div>
+          </div>
+
           <div>
             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
               Email address
@@ -30,11 +48,6 @@ function Signup() {
               <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                 Password
               </label>
-              <div className="text-sm">
-                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                  Forgot password?
-                </a>
-              </div>
             </div>
             <div className="mt-2">
               <input
@@ -50,20 +63,38 @@ function Signup() {
           </div>
 
           <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password-confirmation" className="block text-sm font-medium leading-6 text-gray-900">
+                Password Confirmation
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password-confirmation"
+                name="password_confirmation"
+                type="password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder='Re-type password'
+              />
+            </div>
+          </div>
+
+          <div>
             <button
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign in
+              Signup
             </button>
           </div>
         </form>
 
         <p className="mt-10 text-center text-sm text-gray-500">
-          Not a member?{' '}
-          <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-            Start a 14 day free trial
-          </a>
+          Already have an account?{' '}
+          <Link to="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            Login with your account
+          </Link>
         </p>
       </div>
     </>
