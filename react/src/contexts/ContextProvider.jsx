bf5f53491817e2faa243c784216126ab60f67ceb
tarequr <tarequrr8@gmail.com>
2024-03-15 21:23:28 +0600

    check token

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -179,8 +179,17 @@ const tmpSurveys = [
 
 export const ContextProvider = ({children}) => {
   const [currentUser, setCurrentUser] = useState({});
-  const [userToken, setUserToken] = useState('');
+  const [userToken, _setUserToken] = useState(localStorage.getItem('TOKEN') || '');
   const [surveys, setSurveys] = useState(tmpSurveys);
+
+  const setUserToken = (token) => {
+    if (token) {
+      localStorage.setItem('TOKEN', token);
+    } else {
+      localStorage.removeItem('TOKEN');
+    }
+    _setUserToken(token);
+  }
 
   return (
     <StateContext.Provider value={{
