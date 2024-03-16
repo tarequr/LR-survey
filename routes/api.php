c39d5ec692683d9ebdeb48e1c65ade0fc93ba820
tarequr <tarequrr8@gmail.com>
2024-03-16 22:15:26 +0600

    feat: survey request created

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::apiResource(SurveyController::class);
+    Route::apiResource('surveys', SurveyController::class);
 });
 
 Route::get('/demo', function() {
