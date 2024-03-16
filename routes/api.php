86991b1e757c9fd1047d828bc83c4f01ad172a2b
tarequr <tarequrr8@gmail.com>
2024-03-16 22:10:12 +0600

    feat: surveycontroller created

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\SurveyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,8 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource(SurveyController::class);
 });
 
 Route::get('/demo', function() {
