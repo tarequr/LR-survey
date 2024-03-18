62cabede38f9759183009a2f45dff724b5414806
tarequr <tarequrr8@gmail.com>
2024-03-18 22:28:05 +0600

    store survey

--- app/Http/Controllers/API/SurveyController.php
+++ app/Http/Controllers/API/SurveyController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\API;
 
 use App\Models\Survey;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use App\Http\Requests\StoreSurveyRequest;
-use App\Http\Requests\UpdateSurveyRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SurveyResource;
+use App\Http\Requests\StoreSurveyRequest;
+use App\Http\Requests\UpdateSurveyRequest;
 
 class SurveyController extends Controller
 {
@@ -28,7 +29,22 @@ class SurveyController extends Controller
      */
     public function store(StoreSurveyRequest $request)
     {
-        //
+        $data = $request->validated();
+
+        if (isset($data['image'])) {
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+        }
+
+        $survey = Survey::create($data);
+
+        // Create new questions
+        foreach ($data['questions'] as $question) {
+            $question['survey_id'] = $survey->id;
+            $this->createQuestion($question);
+        }
+
+        return new SurveyResource($survey);
     }
 
     /**
@@ -53,5 +69,35 @@ class SurveyController extends Controller
     public function destroy(Survey $survey)
     {
         //
+    }
+
+    private function saveImage($image)
+    {
+        // Check if the image is valid base64 string
+        if (preg_match('/^data:image\/(\w+);base64,/', $image, $type)) {
+            // Take out the base64 encoded text without mime type
+            $image = substr($image, strpos($image, ',') + 1);
+            // Get file extension
+            $type = strtolower($type[1]); // jpg, png, gif
+
+            // Check if file is an image
+            if (!in_array($type, ['jpg', 'jpeg', 'png', 'gif'])) {
+                throw new \Exception('Invalid image type');
+            }
+            $image = str_replace(' ', '+', $image);
+            $image = base64_decode($image);
+
+            if ($image === false) {
+                throw new \Exception('base64_decode failed');
+            }
+        } else {
+            throw new \Exception('Did not match data URI with image data');
+        }
+
+        $dir = 'images/';
+        $file = Str::random() . '.' . $type;
+        $absulatePath = public_path($dir);
+        $relativePath = $dir . $file;
+
     }
 }
