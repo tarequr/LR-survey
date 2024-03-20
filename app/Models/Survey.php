ca3d9aa7db4eccb54a651a6a22ad2662c93574a0
tarequr <tarequrr8@gmail.com>
2024-03-20 21:03:17 +0600

    proctected guarded model

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Survey extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
 }
--- app/Models/SurveyAnswer.php
+++ app/Models/SurveyAnswer.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class SurveyAnswer extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
 }
